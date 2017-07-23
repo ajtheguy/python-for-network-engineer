fdd23edd2c4020cdc0af9f2cd99fe5d8c587cc9a
root <ajtheguy@gmail.com>
2017-07-23 04:22:15 +0000

    finished part13

---
+++ part12netmiko.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+from netmiko import ConnectHandler
+
+#Ask for username and password
+user = raw_input("Enter your remote account: ")
+password = getpass.getpass()
+
+iosv_l2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': user,
+    'password': password,
+}
+
+
+net_connect = ConnectHandler(**iosv_l2)
+#net_connect.find_prompt()
+output = net_connect.send_command('show ip int brief')
+print output
+
+config_commands = ['int loop 0', 'ip address 1.1.1.1 255.255.255.0']
+output = net_connect.send_config_set(config_commands)
+print output
+
+for n in range (2,21):
+    print "Creating VLAN " + str(n)
+    config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+    output = net_connect.send_config_set(config_commands)
+    print output 
